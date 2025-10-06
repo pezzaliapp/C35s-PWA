34e727d68ee596f774949856cc0ba3e8e13fe6c0
pezzaliapp <pezzalialessandro@gmail.com>
2025-10-06 17:21:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,5 +2,5 @@
 const CACHE='c35s-pwa-v4';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./data/CASCOS_LIFTS.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(self.clients.claim()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))) .then(self.clients.claim()))});
 self.addEventListener('fetch',e=>{ const url = new URL(e.request.url); if (url.origin === location.origin) { e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); } });
